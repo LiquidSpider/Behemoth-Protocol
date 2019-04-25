3b8dee8e09a0e462f55d91066944cf82e0c2ca0b
Sebastien <sebway116@gmail.com>
2019-04-25 14:05:46 +1000

    Cruising
    Added """"sprint"""" function

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,13 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;               // Player body
     public GameObject body;             // Player gameobject body
     private Animator animator;          // Body animator
+    private TrailRenderer trail;        // Trail renderer in player body
     //  Camera Vars
     public float speedH = 2.0f;         // Sensitivity horizontal
     public float speedV = 2.0f;         // Sensitivity vertical
     public float maxY = 89.9f;          // Clamps player's vertical angle betwwen this
     public float minY = -89.9f;         // And this
+    public float cruiseLMod = 0.5f;     // Multiplier for look speed when cruising
 
     private float rotX = 0.0f;          // Current rotation, don't touch this
     private float rotY = 0.0f;          // Ditto
@@ -20,13 +22,17 @@ public class PlayerController : MonoBehaviour
     public float accSpeed = 200.0f;     // How much force is applied each tick
     public float dTime = 1f;            // How long the player is "dodging" for
     private bool isDodge = false;       // Is the player dodging
-    public float dForce = 10000.0f;      // How much force is applied on dodge
+    public float dForce = 10000.0f;     // How much force is applied on dodge
+    private bool isCruising = false;    // Is the player afterburning
+    public float cruiseMod = 0.10f;     // Multiplier for all movement except forward when cruising
+    public float cruiseFwd = 5f;        // Multiplier for forward movement when cruising
 
     //  Weapon vars
     public GameObject arm;              // Object that holds the gun
 
     void Start() {
         rb = GetComponent<Rigidbody>();
+        trail = body.GetComponent<TrailRenderer>();
         animator = body.GetComponent<Animator>();
         Cursor.lockState = CursorLockMode.Locked;
     }
@@ -38,10 +44,17 @@ public class PlayerController : MonoBehaviour
         AimWeapon();
     }
     void CameraMove() { 
-        rotX += speedH * Input.GetAxis("RightHorizontal");
-        rotY -= speedV * Input.GetAxis("RightVertical");
-        rotX += speedH * Input.GetAxis("Mouse X");
-        rotY -= speedV * Input.GetAxis("Mouse Y");
+        if (!isCruising) {
+            rotX += speedH * Input.GetAxis("RightHorizontal");
+            rotY -= speedV * Input.GetAxis("RightVertical");
+            rotX += speedH * Input.GetAxis("Mouse X");
+            rotY -= speedV * Input.GetAxis("Mouse Y");
+        } else {
+            rotX += speedH * Input.GetAxis("RightHorizontal") * cruiseLMod;
+            rotY -= speedV * Input.GetAxis("RightVertical") * cruiseLMod;
+            rotX += speedH * Input.GetAxis("Mouse X") * cruiseLMod;
+            rotY -= speedV * Input.GetAxis("Mouse Y") * cruiseLMod;
+        }
         rotY = Mathf.Clamp(rotY, minY, maxY);
         transform.eulerAngles = new Vector3(rotY, rotX, 0.0f);
     }
@@ -50,12 +63,28 @@ public class PlayerController : MonoBehaviour
             Cursor.lockState = CursorLockMode.None;
         }
         if (!isDodge) {
-            rb.AddForce(transform.forward * Input.GetAxis("LeftVertical") * accSpeed);
-            rb.AddForce(transform.right * Input.GetAxis("LeftHorizontal") * accSpeed);
-            rb.AddForce(transform.up * Input.GetAxis("AscDesc") * accSpeed);
+            float mods = 1f;
+            if (isCruising) {
+                mods = cruiseMod;
+                rb.AddForce(transform.forward * accSpeed * cruiseFwd);
+            } else {
+                mods = 1f;
+                rb.AddForce(transform.forward * Input.GetAxis("LeftVertical") * accSpeed * mods);
+            }
+            rb.AddForce(transform.right * Input.GetAxis("LeftHorizontal") * accSpeed * mods);
+            rb.AddForce(transform.up * Input.GetAxis("AscDesc") * accSpeed * mods);
         }
         if (Input.GetButtonDown("Dodge") && !isDodge) {
             StartCoroutine(Dodge());
+        }
+        if (Input.GetAxis("Cruise") > 0 && !isDodge) {
+            trail.time = 2;
+            trail.startWidth = 1f;
+            isCruising = true;
+        } else {
+            trail.time = 0.5f;
+            trail.startWidth = 0.5f;
+            isCruising = false;
         }
     }
     void AimWeapon() {
