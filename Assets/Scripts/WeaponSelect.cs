23e7f13e54c9fd7c6f067a7032c59436989a10fd
JR <joshua.rosentreter6@gmail.com>
2019-05-06 16:01:02 +1000

    UI Improvement
    I changed the UI to a prefab so that it can be changed later without ruining the scene. I also made it so that the left section changes with weapon select.

---
+++ Assets/Scripts/WeaponSelect.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class WeaponSelect : MonoBehaviour {
+
+	public Sprite left1;
+	public Sprite left2;
+	public Sprite left3;
+	public Sprite left4;
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetButtonDown("Weapon1")) {
+			gameObject.GetComponent<Image>().sprite = left1;
+		}
+
+		if (Input.GetButtonDown("Weapon2")) {
+			gameObject.GetComponent<Image>().sprite = left2;
+		}
+
+		if (Input.GetButtonDown("Weapon3")) {
+			gameObject.GetComponent<Image>().sprite = left3;
+		}
+
+		if (Input.GetButtonDown("Weapon4")) {
+			gameObject.GetComponent<Image>().sprite = left4;
+		}
+	}
+}
