3ff371523ce217b60c1e728f5139cf2b6eb493f1
Sebastien <sebway116@gmail.com>
2019-05-16 21:30:18 +1000

    UI Crosshair

--- Assets/Scripts/UIScripts/Crosshair.cs
+++ Assets/Scripts/UIScripts/Crosshair.cs
@@ -5,11 +5,18 @@ using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour
 {
-    public GameObject notchU;			
-    public GameObject notchD;
-    public GameObject notchL;
-    public GameObject notchR;
-    
+    public GameObject notchU;
+    public GameObject notchBR;
+    public GameObject notchBL;
+    private Vector3 oNotchU;
+    private Vector3 oNotchBR;
+    private Vector3 oNotchBL;
+
+    void Start() {  // Get starting position
+        oNotchU = notchU.GetComponent<RectTransform>().anchoredPosition;
+        oNotchBR = notchBR.GetComponent<RectTransform>().anchoredPosition;
+        oNotchBL = notchBL.GetComponent<RectTransform>().anchoredPosition;
+    }
     void Update()
     {
         GunTemplate gun = null;
@@ -20,10 +27,9 @@ public class Crosshair : MonoBehaviour
         }
 
         if (gun) {
-            notchU.GetComponent<RectTransform>().anchoredPosition = Vector3.up * (20f + (gun.cAcc * 0.50f));
-            notchD.GetComponent<RectTransform>().anchoredPosition = Vector3.down * (20f + (gun.cAcc * 0.50f));
-            notchL.GetComponent<RectTransform>().anchoredPosition = Vector3.left * (20f + (gun.cAcc * 0.50f));
-            notchR.GetComponent<RectTransform>().anchoredPosition = Vector3.right * (20f + (gun.cAcc * 0.50f));
+            notchU.GetComponent<RectTransform>().anchoredPosition = oNotchU + Vector3.up * (gun.cAcc * 0.75f);
+            notchBR.GetComponent<RectTransform>().anchoredPosition = oNotchBR + Vector3.down * (gun.cAcc * 0.50f) + Vector3.right * (gun.cAcc * 0.75f);
+            notchBL.GetComponent<RectTransform>().anchoredPosition = oNotchBL + Vector3.down * (gun.cAcc * 0.50f) + Vector3.left * (gun.cAcc * 0.75f);
         }
     }
 }
