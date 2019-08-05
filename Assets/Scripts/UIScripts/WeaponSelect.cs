f0e216b8a2ee598f294025d16afc052c8c7db11e
Jonathan <jonathan.dragoon@hotmail.com>
2019-08-05 13:41:19 +1000

    New UI and Animations
    +UI Animations
    ~UI Look
    -Old UI Images

--- Assets/Scripts/UIScripts/WeaponSelect.cs
+++ Assets/Scripts/UIScripts/WeaponSelect.cs
@@ -6,15 +6,22 @@ using UnityEngine.UI;
 
 public class WeaponSelect : MonoBehaviour {
 
-	public Sprite left1;
-	public Sprite left2;
-	public Sprite left3;
+	public GameObject weapCirc1;
+	public GameObject weapBar1;
+	public GameObject weapCirc2;
+	public GameObject weapBar2;
+	public GameObject weapCirc3;
+	public GameObject weapBar3;
+
+	public GameObject missile1;
+	public GameObject missile2;
 
 	public int weaponNumber = 1;
 	public int previousWeaponNumber = 1;
 
 	private void Start() {
-		gameObject.GetComponent<Image>().sprite = left1;
+		weapCirc1.GetComponent<Animator>().SetBool("Active", true);
+		weapBar1.GetComponent<Animator>().SetBool("Active", true);
 	}
 
 	// Update is called once per frame
@@ -22,19 +29,46 @@ public class WeaponSelect : MonoBehaviour {
 		if (Input.GetButtonDown("Weapon1")) {
 			previousWeaponNumber = weaponNumber;
 			weaponNumber = 1;
-			gameObject.GetComponent<Image>().sprite = left1;
+			//play animations here
+			weapCirc1.GetComponent<Animator>().SetBool("Active", true);
+			weapBar1.GetComponent<Animator>().SetBool("Active", true);
+			weapCirc2.GetComponent<Animator>().SetBool("Active", false);
+			weapBar2.GetComponent<Animator>().SetBool("Active", false);
+			weapCirc3.GetComponent<Animator>().SetBool("Active", false);
+			weapBar3.GetComponent<Animator>().SetBool("Active", false);
+
+			missile1.GetComponent<Animator>().SetBool("Active", false);
+			missile2.GetComponent<Animator>().SetBool("Active", false);
 		}
 
 		if (Input.GetButtonDown("Weapon2")) {
 			previousWeaponNumber = weaponNumber;
 			weaponNumber = 2;
-			gameObject.GetComponent<Image>().sprite = left2;
+			//play animations here
+			weapCirc1.GetComponent<Animator>().SetBool("Active", false);
+			weapBar1.GetComponent<Animator>().SetBool("Active", false);
+			weapCirc2.GetComponent<Animator>().SetBool("Active", true);
+			weapBar2.GetComponent<Animator>().SetBool("Active", true);
+			weapCirc3.GetComponent<Animator>().SetBool("Active", false);
+			weapBar3.GetComponent<Animator>().SetBool("Active", false);
+
+			missile1.GetComponent<Animator>().SetBool("Active", true);
+			missile2.GetComponent<Animator>().SetBool("Active", true);
 		}
 
 		if (Input.GetButtonDown("Weapon3")) {
 			previousWeaponNumber = weaponNumber;
 			weaponNumber = 3;
-			gameObject.GetComponent<Image>().sprite = left3;
+			//play animations here
+			weapCirc1.GetComponent<Animator>().SetBool("Active", false);
+			weapBar1.GetComponent<Animator>().SetBool("Active", false);
+			weapCirc2.GetComponent<Animator>().SetBool("Active", false);
+			weapBar2.GetComponent<Animator>().SetBool("Active", false);
+			weapCirc3.GetComponent<Animator>().SetBool("Active", true);
+			weapBar3.GetComponent<Animator>().SetBool("Active", true);
+
+			missile1.GetComponent<Animator>().SetBool("Active", false);
+			missile2.GetComponent<Animator>().SetBool("Active", false);
 		}
 	}
 }
