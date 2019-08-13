4df121a16249ad933afc3949744836e81cabaf7f
JR <joshua.rosentreter6@gmail.com>
2019-08-14 02:52:11 +1000

    Controller Controls Screen Added
    + The panel in the instructions menu that has the controller controls (and accompanying code to transition)

---
+++ Assets/Scripts/UIScripts/InstructionsChange.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class InstructionsChange : MonoBehaviour {
+	public Sprite mkControls;
+	public Sprite cControls;
+
+	public void MKControls() {
+		gameObject.GetComponent<Image>().sprite = mkControls;
+
+		transform.GetChild(0).gameObject.SetActive(false);
+		transform.GetChild(1).gameObject.SetActive(true);
+	}
+
+	public void CControls() {
+		gameObject.GetComponent<Image>().sprite = cControls;
+
+		transform.GetChild(0).gameObject.SetActive(true);
+		transform.GetChild(1).gameObject.SetActive(false);
+	}
+
+}
