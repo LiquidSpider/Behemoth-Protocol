c1436f815b3b3081dc718be35e5ce715e9f84c5e
Seb <Seb>
2019-08-28 21:23:04 +1000

    New player model
    + New player model
    + Animation layers

--- Assets/Scripts/PlayerScripts/AnimatorManager.cs
+++ Assets/Scripts/PlayerScripts/AnimatorManager.cs
@@ -5,24 +5,31 @@ using UnityEngine;
 public class AnimatorManager : MonoBehaviour
 {
     Animator animator;
+    Rigidbody rb;
+    ShieldBehaviour shield;
+    Vector2 inputVector;
     // Start is called before the first frame update
     void Start()
     {
+        shield = FindObjectOfType<ShieldBehaviour>();
+        rb = FindObjectOfType<PlayerController>().gameObject.GetComponent<Rigidbody>();
         animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) ||
-            Input.GetKey(KeyCode.S) ||
-            Input.GetKey(KeyCode.D) ||
-            Input.GetKey(KeyCode.W) ||
-            Input.GetKey(KeyCode.Space) ||
-            Input.GetKey(KeyCode.LeftControl))
+        if (Input.GetAxis("LeftVertical") != 0 || Input.GetAxis("LeftHorizontal") != 0 || Input.GetAxis("AscDesc") != 0)
         {
             animator.SetBool("IsMoving", true);
         }
         else animator.SetBool("IsMoving", false);
+        inputVector.x = Input.GetAxis("LeftHorizontal");
+        inputVector.y = Input.GetAxis("LeftVertical");
+        animator.SetLayerWeight(1, Mathf.Lerp(animator.GetLayerWeight(1), inputVector.normalized.x, 0.1f));
+        animator.SetLayerWeight(2, Mathf.Lerp(animator.GetLayerWeight(2), -inputVector.normalized.x, 0.1f));
+        animator.SetLayerWeight(3, Mathf.Lerp(animator.GetLayerWeight(3), -inputVector.normalized.y, 0.1f));
+        animator.SetLayerWeight(4, Mathf.Lerp(animator.GetLayerWeight(4), inputVector.normalized.y, 0.1f));
+        animator.SetBool("IsShielding", shield.shieldActive);
     }
 }
