58d00f8136415048d69e4c5ab82acc6ca57160e5
Seb <Seb>
2019-08-31 10:06:15 +1000

    Vacuum sounds
    + Vacuum now has sounds

---
+++ Assets/Scripts/Audio/VacuumSound.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VacuumSound : MonoBehaviour
+{
+    PlayerHealth player;
+    AudioSource audio;
+    [SerializeField]
+    AudioClip start;
+    [SerializeField]
+    AudioClip middle;
+    [SerializeField]
+    AudioClip end;
+    bool hasStarted = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerHealth>();
+        audio = GetComponentInChildren<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.isVacuuming)
+        {
+            if (!hasStarted)
+            {
+                audio.loop = false;
+                audio.clip = start;
+                audio.Play();
+                hasStarted = true;
+            }
+            if (audio.clip == start && !audio.isPlaying)
+            {
+                audio.clip = middle;
+                audio.loop = true;
+                audio.Play();
+            }
+        }
+        if (!player.isVacuuming && hasStarted)
+        {
+            audio.loop = false;
+            audio.clip = end;
+            audio.Play();
+            hasStarted = false;
+        }
+    }
+}
